8e08d06f85a94d61137f0982ec98aaa3120b8d49
Lee Copland <lee@leecopland.com>
2025-11-27 18:24:23 +1100

    Adding documentation and basis of API

---
+++ backend/README.md
@@ -0,0 +1,15 @@
+# Not complete, just jotting notes
+
+Activate virtual environment
+
+```bash
+source .venv/bin/activate
+```
+
+To run the fastapi server:
+
+```bash
+fastapi dev main.py
+```
+
+Navigate to `http://127.0.0.1:8000 # default` in browser
--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,68 @@
+from typing import Union
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+# from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React dev server
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+# Just a test for now, using tutorial from FastAPI docs
 @app.get("/")
 def read_root():
-    # Just placeholder for now
-    return {"message": "Test"}
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+
+
+# Example schema endpoints:
+# WIP
+
+
+# Farm related endpoints
+@app.post("/farms/single")
+def submit():
+    return
+
+
+@app.post("/farms/bulk")
+def submit_bulk():
+    return
+
+
+@app.get("/farms/{farm_id}")
+def retrieve_id():
+    return
+
+
+@app.put("/farms/{farm_id}")
+def update_farm():
+    return
+
+
+@app.get("/farms/by-supervisor/{supervisor_id}")
+def retrieve_farms():
+    return
+
+
+# Species related endpoints
+@app.get("species/approved")
+def approved_list():
+    return
+
+
+# ETC
