2a4535fd26a7bd1640b4828fb24a1ecdaeea2deb
Darrin Stephens <dwstephens75@gmail.com>
2025-12-11 21:04:13 +1100

    Suitability scoring performance improvements (#55)
    * Suitability scoring module layout
    
    Some empty placeholder files with the layout for the  suitability scoring. This may change.
    
    * WIP: SSE requirements and data dictionary docs
    
    * Update data_dictionary and requirements
    
    * First draft of output schema
    
    * Remove file as it was informational
    
    * Update requirements to mention initial approach
    
    * Update data dictionary to include area
    
    This might be used by the sapling calculator
    
    * Add datasets for farm and species for model development
    
    * Fix typo in file
    
    * Add ipykernel and openpyxl packages
    
    * Fix type in species file
    
    * Fix spelling of riparian
    
    * Add local gitignore to datascience
    
    * Add adjusttext package to pyproject.toml
    
    * Fix spelling of coastal in farms.xlsx
    
    * Add species EDA/Clustering notebook
    
    * Add farms EDA/clustering notebook and cleaned farms.xlsx file
    
    * Add exclusion_rules and data folder
    
    * Move species and farms data files into the datascience/data folder
    
    * Update species EDA notebook to the new location for the data.
    
    * Remove placeholder file
    
    * Update farms EDA to use new data location
    
    This also adds a cleaned version of the farms dataset
    
    * Suitability scoring module layout
    
    Some empty placeholder files with the layout for the  suitability scoring. This may change.
    
    * WIP: SSE requirements and data dictionary docs
    
    * Update data_dictionary and requirements
    
    * Remove file as it was informational
    
    * Re-created uv.lock
    
    * Fix after linting
    
    * Sort the farms_cleaned.xlsx by farm_id
    
    * Fix spelling of coastal
    
    * Remove unused packages
    
    * Remove unused packages and add pytest spec
    
    * Remove place holder file
    
    * Create folder and add place holder for exclusion_rules module
    
    * Add src for suitability scoring
    
    * Add  numerical_range tests
    
    * Update datascience gitignore
    
    * Add tests for parse_prefs
    
    * Add tests for categorical_exact_score
    
    * Add tests for subseting and ruff format other test files
    
    * Fix: Bug found when testing
    
    If the subset dataframe is empty, the reporting of unknow ids was missing.
    
    * Fix: bug found during testing
    
    Tried to append to a dictionary rather than a list.
    
    * Fix: Typos found during testing
    
    * Add tests for scoring function
    
    * Formatting change
    
    * Rename files to prevent clash with exclusion tests
    
    * Update comments
    
    * Tests floats using the pytest.approx function
    
    * Add comments to test file
    
    * Add comments to test file
    
    * Use pytest.approx for float comparison
    
    * Add tests for config file reading
    
    * Removed unused import
    
    * Update test to use fixtures
    
    * Add tests for utils
    
    * Fix error found during testing
    
    * Add recommender tests
    
    * Update docs for scoring fucntion
    
    * Update scoring design document
    
    * Fix: Incorrect handling of missing parameters
    
    Handled None correctly but not np.nan
    
    * Fix: scoring documentation
    
    * Formatting
    
    Forgot to run ruff
    
    * ENH: Remove subset dataframe and replace with dictionary
    
    This removes the bottle neck of sub-setting a dataframe within the farm loop
    
    * ENH: Relace farm dataframe with dictionary
    
    Removes costly iterrows() call.
    
    * Remove commented code
    
    * Update recommend app to allow easy creation of the farm ids list for testing
    
    * ENH: pre-compute rules.
    
    This moves get_feature_params outside of the inner loop.

--- datascience/src/recommend_app.py
+++ datascience/src/recommend_app.py
@@ -3,10 +3,7 @@ from suitability_scoring.recommend import build_payloads_for_farms
 
 def main():
     """ """
-    farm_ids = [
-        1,
-    ]
-
+    farm_ids = list(range(1, 1001))
     output = build_payloads_for_farms(farm_ids)
     print(output)
 
