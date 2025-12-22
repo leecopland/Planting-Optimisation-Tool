6e23b9d156bbee8047f31ad288c6503a9ec66915
Lee Copland <lee@leecopland.com>
2025-12-22 23:00:40 +1100

    Forgot to format DS files I changed

--- datascience/src/suitability_scoring/scoring/__init__.py
+++ datascience/src/suitability_scoring/scoring/__init__.py
@@ -1,4 +1,6 @@
 from .scoring import score_farms_species_by_id_list, mcda_scorer
 
-__all__ = ["score_farms_species_by_id_list",
-            "mcda_scorer",]
+__all__ = [
+    "score_farms_species_by_id_list",
+    "mcda_scorer",
+]
--- datascience/src/suitability_scoring/utils/__init__.py
+++ datascience/src/suitability_scoring/utils/__init__.py
@@ -1,4 +1,6 @@
 from .params import build_species_params_dict, get_feature_params
 
-__all__ = ["build_species_params_dict",
-           "get_feature_params",]
+__all__ = [
+    "build_species_params_dict",
+    "get_feature_params",
+]
