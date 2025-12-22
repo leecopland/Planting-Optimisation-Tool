30797a2631d2f4d1264afed69949865799e418f5
Lee Copland <lee@leecopland.com>
2025-12-23 00:16:24 +1100

    Recommendation test passed

--- backend/src/services/species.py
+++ backend/src/services/species.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import selectinload
 from suitability_scoring.utils.config import load_yaml
 from src.models.species import Species
 
+
 def get_recommend_config():
     # This is a very ugly workaround, which i'm only committing so that I can get a successful test recommendation.
     # It desperately needs to be refactored to not be so ugly in future.
@@ -13,12 +14,13 @@ def get_recommend_config():
     # Go up 3 levels to get to .../datascience/
     base_path = Path(suitability_scoring.__file__).resolve().parent.parent.parent
     config_path = base_path / "config" / "recommend.yaml"
-    
+
     if not config_path.exists():
         # This will say where it looked so it can be debugged if it fails
         raise FileNotFoundError(f"YAML not found! Looked in: {config_path}")
-        
+
     return load_yaml(str(config_path))
+
 
 async def get_all_species_for_engine(db: AsyncSession):
     stmt = select(Species).options(selectinload(Species.soil_textures))
